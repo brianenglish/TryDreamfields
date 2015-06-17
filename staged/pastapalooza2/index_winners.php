ae02c39e93514cddd7d3a7c13413b14427a0b71c
brianenglish <brianenglish@cdd06ec2-aa1c-73e0-859a-ce57b7db7bff>
2015-06-17 18:46:05 +0000

    edits to contest page
    git-svn-id: https://github.com/Hyperdrivei/dreamfields-trydreamfields.com.git/trunk@6 cdd06ec2-aa1c-73e0-859a-ce57b7db7bff

---
+++ staged/pastapalooza2/index_winners.php
@@ -0,0 +1,166 @@
+<?php
+
+/**- - - - - - - - - - - - - - - - - - - - *
+ * These vars affect the green signup form
+ * - - - - - - - - - - - - - - - - - - - - */
+//$thx = "http://www.trydreamfields.com/coupon-thanks-LBB.php";
+//$lid = "18633454";
+//$lid_comment = "(2015/01/28 ... My Lists > 2010_Landing_Pages > TryDrm_Landing_Pages > LittleBlackBox";
+//$group = ""; // default is Eat Healthier (other options: Managing Diabetes, Low Carb Diet)
+//$source = "TryDrm-LittleBlackBox_2015";
+
+//$revtrax_source = "TryDreamfields-LLB";
+//$campaign = "PinterestLBB";
+//$trigKey = "DRM220_TryDrmSignup_Welcome"; // name of triggered send
+
+?>
+<?php 
+$pagetitle = "PASTAPALOOZA V – A Market Basket Challenge to Help Fight Hunger";
+//$metadescription = "asdf";
+//$keywords = "asdf";
+
+$og_title = "PASTAPALOOZA V";
+$og_description = "A Market Basket Challenge to Help Fight Hunger";
+//$og_image = "";
+
+
+$stylesheet = "home_2015_05_07.css";
+define ('JS_SHEET', 'DRM270_Pastapalooza');
+$bodyClass = "home"
+?>
+<?php include('../_includes/DRM270_Pastapalooza/subdir_header.php'); ?>
+
+<script type="text/javascript">
+(function(d, s, id) {
+  window.Wishpond = window.Wishpond || {};
+  Wishpond.merchantId = '255689';
+  var js, wpjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//cdn.wishpond.net/connect.js";
+  wpjs.parentNode.insertBefore(js, wpjs);
+}(document, 'script', 'wishpond-connect'));
+</script>
+
+	<div class="site_body">
+	<div id="border-gold">
+		
+		
+		<!-- BEGIN page content -->
+		<div id="hero">
+			<img src="../_img/DRM270_Pastapalooza/DRM270_hero.jpg" class="img1" alt="Pastapalooza V" title="Pastapalooza V">
+			<img src="../_img/DRM270_Pastapalooza/DRM270_hero_med.jpg" class="img2" alt="Pastapalooza V" title"Pastapalooza V">
+		</div>
+        
+        
+
+		<div class="main-content">
+		
+			
+            <div class="ca_1">
+                
+				<div>
+	                <p>
+	                To celebrate the fifth anniversary of Dreamfields&rsquo; delicious salute to summer, we&rsquo;re working with leading food bloggers to create exciting pasta recipes to enjoy on warm summer days&hellip;and help raise much needed support for local food banks.
+	                </p>
+                    
+                    
+                    <ul class="main">
+                      <li>Throughout the summer, there will be five separate &ldquo;Market Basket Challenges&rdquo; where bloggers  are asked to use all the ingredients in a Market Basket (plus up to 2 of their  choice) to create original pasta salad, side dish or summer entr&eacute;e  recipes.</li>
+                      <li>We&rsquo;re  calling on all our fans, friends and followers to vote for the recipe they like  best during each challenge. You can cast  one vote per day for the recipe of your choice.  Each challenge will last one week.</li>
+                      <li>The winning recipe/blogger will receive a $1,000 donation made in their name to their local food bank. A $100 donation will also be made to the local food bank of each runner-up.</li>
+                      <li>PLUS &ndash; one voter from each Market Basket Challenge will be chosen at random to win a  family pack of Dreamfields pasta and a $1,000 donation to the food bank of <em>their</em> choice. So the more you vote, the  more chances you have to win.</li>
+                    </ul>
+                  
+					<div class="buttons">
+                        <span class="one">
+                        <a href="rules.php" class="btn-blue">View Contest Rules <span class="arrow"></span></a> 
+                        </span>
+    
+                        <span class="two">
+                        <a href="blog-post.php" class="btn-white">Learn More <span class="arrow"></span></a> 
+                        <a href="blog-post.php">Find out why hunger doesn't take a vacation<br class="hideBR"> in summer by clicking here</a>. 
+                        </span>
+					</div>
+
+				</div>
+                
+            </div><!-- ca_1a -->
+            
+            <hr>
+            
+			<div class="ca_1">
+				
+				<h1 class="larger">Garden Fresh Basket May 21<sup>st</sup></h1>
+                
+                <p>
+                This week&rsquo;s challenge features the best farm- and garden-fresh ingredients around, including: corn, tomatoes, cucumbers, greens, and herbs. The blogger participants may add up to two ingredients of their choice to round out their dish. Dreamfields pasta, salt, pepper, oil and water are &ldquo;free ingredients.&rdquo;
+                </p>
+                
+                <p>
+                To see the recipes, click on the images. To vote for your favorite one(s), enter your email and click on the &ldquo;VOTE&rdquo; button next to the image. Remember, while you can vote for more than one recipe, you can only cast one vote per day. Voting for this challenge ends at midnight EDT, May 27, 2015.
+                </p>
+                
+                
+                <div class="wishpond-campaign" data-wishpond-id="745034" data-wishpond-href="https://www.wishpond.com/lp/745034/"></div>
+                
+
+			</div><!-- ca_1b -->
+			
+            <hr>
+            
+			<div class="ca_1">
+				
+				<h1 class="larger">Coming Up Next&hellip;</h1>
+                
+                <p style="text-align: center;">
+                 The Guy&rsquo;s Challenge Basket. Voting starts on June 11, 2015.
+                </p>
+                
+                <p>&nbsp;</p>
+
+			</div><!-- ca_1c -->
+			
+			<div class="ca_2 ca_special">
+				
+				<div class="ftr_thumbs">
+					<a href="http://dreamfieldsfoods.com/healthy-pasta-recipes.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_BrowseRecipes.gif" alt="Browse Recipes" title="Browse Recipes" />
+							<span>Browse <br class="show_mobile">Recipes</span>
+						</div>
+					</a>
+					<a href="http://dreamfieldsfoods.com/find-dreamfields-pasta.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_FindAStore.gif" alt="Find A Store" title="Find A Store" />
+							<span>Find A <br class="show_mobile">Store</span>
+						</div>
+					</a>
+					<a href="http://www.buydreamfields.com/" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_BuyOnline.gif" alt="Buy Online" title="Buy Online" />
+							<span>Buy Online</span>
+						</div>
+					</a>
+					<a href="http://dreamfieldsfoods.com/healthy-carb-pasta.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_LearnMore.gif" alt="Learn More" title="Learn More" />
+							<span>Learn More</span>
+						</div>
+					</a>
+				</div><!-- ftr_thumbs -->
+				
+
+			</div><!-- ca_2 ca_special -->
+		
+		
+		</div>
+		<!-- END page content -->
+		
+
+	</div><!-- #border_gold -->
+	</div><!-- #site_body -->
+
+<?php include('../_includes/DRM270_Pastapalooza/subdir_footer.php'); ?>	
+</body>
+</html>
