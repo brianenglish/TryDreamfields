044f106cee0324aadf2df02364da89a31df92ad2
brianenglish <brianenglish@cdd06ec2-aa1c-73e0-859a-ce57b7db7bff>
2015-08-18 14:29:15 +0000

    United Lander page created
    git-svn-id: https://github.com/Hyperdrivei/dreamfields-trydreamfields.com.git/trunk@13 cdd06ec2-aa1c-73e0-859a-ce57b7db7bff

---
+++ _includes/DRM_United/subdir_green_form.php
@@ -0,0 +1,119 @@
+<?php 
+$adgroup = (!empty($_GET['adgroup']) ) ? $_GET['adgroup'] : "";
+$matchtype = (!empty($_GET['matchtype']) ) ? $_GET['matchtype'] : "";
+$keyword = (!empty($_GET['keyword']) ) ? $_GET['keyword'] : "";
+
+$bannersize = (!empty($_GET['bannersize']) ) ? $_GET['bannersize'] : "";
+$site = (!empty($_GET['site']) ) ? $_GET['site'] : "";
+$placementid = (!empty($_GET['placementid']) ) ? $_GET['placementid'] : "";
+$segmentid = (!empty($_GET['segmentid']) ) ? $_GET['segmentid'] : "";
+?>
+<div class="outer_greenbox">
+    <div class="greenbox">
+		
+		<img src="../_img/home/home_groupshot.png" width="442" height="180">
+		
+<?php
+	if($form_headline == "") {
+?>
+		<h1>Get $1.00 Off Any Box of Dreamfields Pasta</h1>
+<?php
+	} else {
+?>
+		<h1><?php echo $form_headline; ?></h1>
+<?php
+	}//endif
+?>		
+        
+		<div id="form">
+			<form action="../_process/lander_proxy_withZip_optin.php" name="subscribeForm" id="subscribeForm" method="post" onSubmit="return checkEmailForm();" >
+				<input type="hidden" name="thx" value="<?php echo ($thx != "") ? $thx : "http://www.trydreamfields.com/coupon-thanks.php" ?>">
+				<input type="hidden" name="err" value="http://www.trydreamfields.com/error.php">
+
+				<!-- <?php echo $lid_comment; ?> -->
+				<input type="hidden" name="lid" value="<?php echo $lid; ?>" />
+
+				<input type="hidden" name="source" value="<?php echo ($source != "") ? $source : "" ?>">
+				<input type="hidden" name="revtrax_source" value="<?php echo ($revtrax_source != "") ? $revtrax_source : "" ?>">
+				<input type="hidden" name="group" value="<?php echo ($group != "") ? $group : "Eat Healthier" ?>">
+				<input type="hidden" name="campaign" value="<?php echo ($campaign != "") ? $campaign : "" ?>">
+				
+<?php
+	if($adgroup != "") {
+?>
+				<input type="hidden" name="adgroup" value="<?php echo ($adgroup != "") ? $adgroup : "" ?>">
+<?php
+	}//endif
+?>
+<?php
+	if($matchtype != "") {
+?>
+				<input type="hidden" name="matchtype" value="<?php echo ($matchtype != "") ? $matchtype : "" ?>">
+<?php
+	}//endif
+?>
+<?php
+	if($keyword != "") {
+?>
+				<input type="hidden" name="keyword" value="<?php echo ($keyword != "") ? $keyword : "" ?>">
+<?php
+	}//endif
+?>
+
+<?php
+	if($bannersize != "") {
+?>
+				<input type="hidden" name="bannersize" value="<?php echo ($bannersize != "") ? $bannersize : "" ?>">
+<?php
+	}//endif
+?>
+<?php
+	if($site != "") {
+?>
+				<input type="hidden" name="site" value="<?php echo ($site != "") ? $site : "" ?>">
+<?php
+	}//endif
+?>
+<?php
+	if($placementid != "") {
+?>
+				<input type="hidden" name="placementid" value="<?php echo ($placementid != "") ? $placementid : "" ?>">
+<?php
+	}//endif
+?>
+<?php
+	if($segmentid != "") {
+?>
+				<input type="hidden" name="segmentid" value="<?php echo ($segmentid != "") ? $segmentid : "" ?>">
+<?php
+	}//endif
+?>
+                
+				<input type="hidden" name="trigKey" value="<?php echo ($trigKey != "") ? $trigKey : "" ?>">
+				<input type="hidden" name="trigger" value="yes">
+				<input type="hidden" name="rec" id="rec" value="">
+				
+				<div class="div-email"><input placeholder="Enter E-mail Address" type="text" name="email" id="email" size="30" value=""></div>
+
+				<div class="clearMe">
+					<div class="div-zip"><input placeholder="Zip Code" type="text" name="zip" id="zip" size="30" value=""></div>
+					<div class="div-coupon_submit">
+                        <button type="submit" class="btn-white">
+                        	Print My Coupon<span class="arrow"></span>
+                        </button>
+                    </div>
+				</div>
+				
+				<div class="unsubChk"><input type="checkbox" name="unsubChk" id="unsubChk" value="Yes"></div>
+				<div class="unsub_txt">Yes, send me monthly emails with delicious recipes, coupon savings and exclusive contests. (<a href="http://www.dreamfieldsfoods.com/privacy-policy.php" target="_blank">Your email is safe with us</a>)</div>
+			
+			</form>
+		</div>
+        
+        <div class="form_disclaimer">
+    	By entering your email address and checking the box above, you are agreeing to receive a monthly email from Dreamfields containing delicious recipes and coupon savings. You can unsubscribe at any time.
+   		</div>
+        
+	</div>
+    
+</div><!-- end .outer_greenbox -->
---
+++ _includes/DRM_United/subdir_header.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="p:domain_verify" content="d1bd41ff653df54197ece78db89fa9c7"/> 
+	<title><?php if ( isset($pagetitle) ) { echo $pagetitle; } ?></title>
+    <?php 
+	// metas
+	if ( isset($metadescription) ) { 
+		echo "	<meta name=\"description\" content=\"" . $metadescription . "\">\n"; 
+	} 
+	// keywords
+	if ( isset($keywords) ) { 
+		echo "	<meta name=\"keywords\" content=\"" . $keywords . "\">\n"; 
+	} 
+	?>
+    
+    <?php 
+	// OG metas
+	if ( isset($og_title) ) { 
+		echo "	<meta property=\"og:title\" content=\"" . $og_title . "\" />\n"; 
+	}
+    if ( isset($og_description) ) { 
+    	echo "	<meta property=\"og:description\" content=\"" . $og_description . "\" />\n"; 
+    } 
+		echo "	<meta property=\"og:url\" content=\"http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "\" />\n";
+	if ( isset($og_image) && ($og_image != "") ) { 
+		echo "	<meta property=\"og:image\" content=\"" . $og_image . "\" />\n"; 
+	} else {
+		echo "	<meta property=\"og:image\" content=\"http://" . $_SERVER['HTTP_HOST'] . "/" . "_img/og_images/og_200x200_default.jpg" . "\" />\n"; 
+	}
+	?>
+    
+	<meta name="viewport" content="width=device-width">
+	
+	<link rel="stylesheet" href="../_css/DRM259/basestyle_2014_12_15.css">
+<?php 
+	$contentCtrl = ( isset($contentCtrl) ) ? $contentCtrl : "" ;
+	if ( $contentCtrl == true ) { 
+		echo "	<link rel=\"stylesheet\" href=\"../_css/DRM259/ContentCTRL.css\" type=\"text/css\" />\n"; 
+	}
+?>
+<?php if ( isset($stylesheet) ) { 
+	echo "	<link rel=\"stylesheet\" href=\"../_css/DRM259/pages/" . $stylesheet . "\">\n"; 
+}
+?>
+<?php 
+// RevTrax coupon code
+// &programId=44624892&affiliateId=12049316&merchantId=12049441
+// Set some default values for RevTrax
+$programId = (!empty($revtrax_programId)) ? $revtrax_programId : "44624892";
+if ( isset($revtrax_js) ) { echo "	<script src=\"http://images.revtrax.com/RevTrax/js/rtxdiyframe.jsp?parent=mydiv&rtxuseqs=true&merchantId=12049441&programId={$programId}&affiliateId=12049316&channel=brand\">
+</script>\n"; } 
+?>
+<?php 
+$prettyphoto = ( isset($prettyphoto) ) ? $prettyphoto : "" ;
+if ( $prettyphoto == true ) { 
+	echo "	<script src=\"../_js/prettyphoto/js/jquery-1.6.1.min.js\" type=\"text/javascript\"></script>";
+	echo "	<link rel=\"stylesheet\" href=\"../_js/prettyphoto/css/prettyPhoto.css\" type=\"text/css\" media=\"screen\" title=\"prettyPhoto main stylesheet\" charset=\"utf-8\" />\n"; 
+	echo "	<script src=\"../_js/prettyphoto/js/jquery.prettyPhoto.js\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+}
+?>
+<?php 
+$flexslider = ( isset($flexslider) ) ? $flexslider : "" ;
+if ( $flexslider === true ) {
+?>
+	<link rel="stylesheet" href="../_js/flexslider/flexslider.css" type="text/css" media="screen" title="flexslider stylesheet" />
+<?php
+}//endif
+?>
+	<script src="../_js/modernizr-2.6.2.min.js"></script>
+    <?php
+    /**
+	 * If $sharethis = true, we load the ShareThis javascript
+	 */
+	if(isset($sharethis) && $sharethis == true) {
+	?>
+    <!-- sharethis -->
+    <script type="text/javascript">var switchTo5x=true;</script>
+	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+	<script type="text/javascript">
+		stLight.options({
+			publisher: "f4078d11-0af1-4091-b34f-110f481e6e75", 
+			doNotHash: false, 
+			doNotCopy: false, 
+			hashAddressBar: false,
+			onhover: false
+		});
+    </script>
+    <?php
+    } //endif
+	?>
+	
+</head>
+<body <?php 
+	echo ( !empty($bodyClass) ) ? "class=\"" . $bodyClass . "\"" : "";
+	$googleMap = ( isset($googleMap) ) ? $googleMap : "" ;
+	echo ( $googleMap == true ) ? "onLoad=\"load()\" onUnload=\"GUnload()\"" : ""; 
+?>>
+	
+	<div class="container">
+      
+        <header class="site_header" role="banner">
+            <div class="site_logo"><a href="index.php">Dreamfields Pasta</a></div>
+        </header>
+        
