b90116f30a4dfa0c4641e6ceb8439b3f32288ca9
brianenglish <brianenglish@hyperdrivei.com>
2015-12-17 09:29:02 -0500

    Merge branch 'master' of https://github.com/Hyperdrivei/dreamfields-trydreamfields.com
    Conflicts:
    	_includes/DRM259/footer.php
    	_includes/footer.php

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,36 +1,7 @@
 <?php
 
 if ($_SERVER['REQUEST_URI'] == '/coupon-thanks.php') {
-  print "<!-- Code for Action: Try Dreamfields Print Coupon Conversion Pixel -->
-<!-- Begin Rocket Fuel Conversion Action Tracking Code Version 9 -->
-<script type='text/javascript'>
-(function() {
-  var w = window, d = document;
-  var s = d.createElement('script');
-  s.setAttribute('async', 'true');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', '//c1.rfihub.net/js/tc.min.js'); 
-  var f = d.getElementsByTagName('script')[0];
-  f.parentNode.insertBefore(s, f);
-  if (typeof w['_rfi'] !== 'function') {
-    w['_rfi']=function() {
-      w['_rfi'].commands = w['_rfi'].commands || [];
-      w['_rfi'].commands.push(arguments);
-    };
-  }
-  _rfi('setArgs', 'ver', '9');
-  _rfi('setArgs', 'rb', '24704');
-  _rfi('setArgs', 'ca', '20723946');
-  _rfi('setArgs', '_o', '24704');
-  _rfi('setArgs', '_t', '20723946');
-  _rfi('track');
-})();
-</script>
-<noscript>
-  <iframe src='http://20723946p.rfihub.com/ca.gif?rb=24704&ca=20723946&_o=24704&_t=20723946&rd=INSERT_FINAL_DESTINATION_URL_HEREREPLACE_ME_WITH_YOUR_CACHE_BUSTING' style='display:none;padding:0;margin:0' width='0' height='0'>
-</iframe>
-</noscript>
-<!-- End Rocket Fuel Conversion Action Tracking Code Version 9 -->";
+  print "";
 } else {
   print "<!-- Code for Action: Try Dreamfields Foods Universal Pixel -->
 <!-- Begin Rocket Fuel Conversion Action Tracking Code Version 9 -->
