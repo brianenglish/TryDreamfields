66daeef308e96d251fada7e358f78b6298276233
Cody Moore <cody@hyperdrivei.com>
2016-03-14 08:04:40 -0400

    added pasta and seafood pinterest lander

---
+++ Pinterest-Pasta-and-Seafood/index.php
@@ -0,0 +1,248 @@
+<?php
+
+/**- - - - - - - - - - - - - - - - - - - - *
+ * These vars affect the green signup form
+ * - - - - - - - - - - - - - - - - - - - - */
+$thx = "http://www.trydreamfields.com/coupon-thanks.php";
+$lid = "18950866";
+$lid_comment = "(2015/04/29 ... My Lists > 2010_Landing_Pages > TryDrm_Landing_Pages > Pinterest-Healthy-Recipes";
+$group = ""; // default is Eat Healthier (other options: Managing Diabetes, Low Carb Diet)
+$source = "TryDrm-pinterest-healthy-recipes";
+$revtrax_source = "Pinterest";
+$campaign = "Pinterest_PromotedHealthyRecipePins";
+$trigKey = "DRM220_TryDrmSignup_Welcome"; // name of triggered send
+?>
+<?php
+$pagetitle = "Sign Up for Email from Deliciously Healthy Dreamfields Pasta";
+//$metadescription = "asdf";
+//$keywords = "asdf";
+
+$og_title = "Sign Up for Email from Deliciously Healthy Dreamfields Pasta";
+$og_description = "Dreamfields Pasta is just right for healthy eating. Get a coupon!";
+//$og_image = "";
+
+
+$stylesheet = "trydrm-facebook.css";
+define ('JS_SHEET', 'home');
+$bodyClass = "home"
+?>
+<?php include('../_includes/DRM259/subdir_header.php'); ?>
+
+<!-- CSS in a custom location is always fun -->
+<link rel="stylesheet" href="/css/pinterestLander.css" media="screen" charset="utf-8">
+	<div class="site_body">
+	<div id="border-gold">
+
+
+		<!-- BEGIN page content -->
+		<div>
+			<img src="/_img/pinterest-pasta-and-seafood/Seafood-Header.jpg">
+		</div>
+
+		<div class="main-content">
+			<div class="ca_1">
+				<h1 class="larger">Eat a Little Better with a Great Tasting Healthy Pasta</h1>
+				<div class="thumbnails">
+					<div class="thumbnail">
+						<div class="circle fiber"><span class="text">5g Fiber</span></div>
+						<div class="brownLines"></div>
+					  <p class="fiber">We add a prebiotic fiber, inulin, sourced from chicory root. Inulin helps promote healthy digestion.</p>
+					</div>
+
+					<div class="thumbnail">
+						<div class="circle protein"><span class="text">7g Protein</span></div>
+						<div class="brownLines"></div>
+						<p>7 grams of protein per serving. This is natural occurring protein in our quality durum wheat.</p>
+					</div>
+
+					<div class="thumbnail">
+						<div class="circle taste"><span class="text">Great Taste</span></div>
+						<div class="brownLines"></div>
+						<p class="taste">Get the same taste and texture of traditional pasta with Dreamfields.<br /><br /></p>
+					</div>
+				</div>
+			</div><!-- ca_1 -->
+			<div class="recipeWrapper">
+				<div class="column">
+					<div class="imageWrapper">
+						<img src="/_img/pinterest-pasta-and-seafood/Bahama-Penne-&-Seared-Mahi-Mahi.jpg" alt="">
+						<div class="recipeOverlay">Bahama Penne & Seared Mahi-Mahi</div>
+						<a data-pin-do="buttonPin" data-pin-color="red" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.dreamfieldsfoods.com%2Fhealthy-pasta-recipes%2F2011%2F07%2Fbahama-penne-seared-mahi-mahi.html&media=http%3A%2F%2Fdreamfieldsfoods.com%2F_img%2Frecipes%2Fwp2014_rec%2Fwp2014_rec_BahamaPenne.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png" /></a>
+					</div>
+					<a href="http://www.dreamfieldsfoods.com/healthy-pasta-recipes/2011/07/bahama-penne-seared-mahi-mahi.html" target="_blank" class="btn-blue" style="cursor: pointer;">Get Recipe<span class="arrow"></span></a>
+				</div>
+				<div class="column">
+					<div class="imageWrapper">
+						<img src="/_img/pinterest-pasta-and-seafood/Spicy-Hoisin-Glazed-Salmon-Spaghetti.jpg" alt="">
+						<div class="recipeOverlay">Spicy Hoisin-Glazed Salmon Spaghetti</div>
+						<a data-pin-do="buttonPin" data-pin-color="red" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.dreamfieldsfoods.com%2Fhealthy-pasta-recipes%2F2013%2F03%2Fspicy-hoisin-glazed-salmon-spaghetti.html&media=http%3A%2F%2Fdreamfieldsfoods.com%2F_img%2Frecipes%2Fwp2014_rec%2Fwp2014_rec_SpicyHoisinSalmon.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png" /></a>
+					</div>
+					<a href="http://www.dreamfieldsfoods.com/healthy-pasta-recipes/2013/03/spicy-hoisin-glazed-salmon-spaghetti.html" target="_blank" class="btn-blue" style="cursor: pointer;">Get Recipe<span class="arrow"></span></a>
+				</div>
+				<div class="column">
+					<div class="imageWrapper">
+						<img src="/_img/pinterest-pasta-and-seafood/Easy-Pantry-Tuna-Penne.jpg" alt="">
+						<div class="recipeOverlay">Easy Pantry Tuna Penne</div>
+						<a data-pin-do="buttonPin" data-pin-color="red" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.dreamfieldsfoods.com%2Fhealthy-pasta-recipes%2F2009%2F03%2Feasy-pantry-tuna-penne.html&media=http%3A%2F%2Fdreamfieldsfoods.com%2F_img%2Frecipes%2Fwp2014_rec%2Fwp2014_rec_EasyPantryTunaPenne.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png" /></a>
+					</div>
+					<a href="http://www.dreamfieldsfoods.com/healthy-pasta-recipes/2009/03/easy-pantry-tuna-penne.html" target="_blank" class="btn-blue" style="cursor: pointer;">Get Recipe<span class="arrow"></span></a>
+				</div>
+				<div class="column">
+					<div class="imageWrapper">
+						<img src="/_img/pinterest-pasta-and-seafood/Ange-hair-con-Tilapia.jpg" alt="">
+						<div class="recipeOverlay">Angel Hair con Tilapia</div>
+						<a data-pin-do="buttonPin" data-pin-color="red" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.dreamfieldsfoods.com%2Fhealthy-pasta-recipes%2F2011%2F03%2Fangel-hair-con-tilapia.html&media=http%3A%2F%2Fdreamfieldsfoods.com%2F_img%2Frecipes%2Fwp2014_rec%2Fwp2014_rec_AngelHairTilapia.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png" /></a>
+					</div>
+					<a href="http://www.dreamfieldsfoods.com/healthy-pasta-recipes/2011/03/angel-hair-con-tilapia.html" target="_blank" class="btn-blue" style="cursor: pointer;">Get Recipe<span class="arrow"></span></a>
+				</div>
+				<div class="column">
+					<div class="imageWrapper">
+						<img src="/_img/pinterest-pasta-and-seafood/Garden-Market-Pasta Salad-with-Smoked-Trout.jpg" alt="">
+						<div class="recipeOverlay">Garden Market Pasta Salad with Smoked Trout</div>
+						<a data-pin-do="buttonPin" data-pin-color="red" data-pin-tall="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.dreamfieldsfoods.com%2Fhealthy-pasta-recipes%2F2014%2F02%2Fgarden-market-pasta-salad-with-smoked-trout.html&media=http%3A%2F%2Fdreamfieldsfoods.com%2F_img%2Frecipes%2Fwp2014_rec%2Fwp2014_rec_GardentMarketPastaSalad.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png" /></a>
+					</div>
+					<a href="http://www.dreamfieldsfoods.com/healthy-pasta-recipes/2014/02/garden-market-pasta-salad-with-smoked-trout.html" target="_blank" class="btn-blue" style="cursor: pointer;">Get Recipe<span class="arrow"></span></a>
+				</div>
+				<div class="column">
+					<a data-pin-do="embedUser" data-pin-board-width="900" data-pin-scale-height="120" data-pin-scale-width="115" href="https://www.pinterest.com/dreamfields/"></a>
+				</div>
+			</div>
+			<img class="pinterestFormHeadline" src="/_img/pinterest-pasta-and-seafood/formHeadline.jpg" alt="Love Dreamfields Recipes? Sign up and get them delivered right to your inbox.">
+			<div class="outer_greenbox">
+			    <div class="greenbox">
+						<div class="flexWrapper">
+							<img src="/_img/pinterest-pasta-and-seafood/drm_lander_pinterest_familyshot.png">
+							<div id="form">
+								<form action="/_process/lander_proxy_withZip_optin.php" name="subscribeForm" id="subscribeForm" method="post" onSubmit="return checkEmailForm();" >
+									<input type="hidden" name="thx" value="<?php echo ($thx != "") ? $thx : "http://www.trydreamfields.com/coupon-thanks.php" ?>">
+									<input type="hidden" name="err" value="http://www.trydreamfields.com/error.php">
+
+									<!-- <?php echo $lid_comment; ?> -->
+									<input type="hidden" name="lid" value="<?php echo $lid; ?>" />
+
+									<input type="hidden" name="source" value="<?php echo ($source != "") ? $source : "" ?>">
+									<input type="hidden" name="revtrax_source" value="<?php echo ($revtrax_source != "") ? $revtrax_source : "" ?>">
+									<input type="hidden" name="group" value="<?php echo ($group != "") ? $group : "Eat Healthier" ?>">
+									<input type="hidden" name="campaign" value="<?php echo ($campaign != "") ? $campaign : "" ?>">
+
+									<?php
+									if($adgroup != "") {
+										?>
+										<input type="hidden" name="adgroup" value="<?php echo ($adgroup != "") ? $adgroup : "" ?>">
+										<?php
+									}//endif
+									?>
+									<?php
+									if($matchtype != "") {
+										?>
+										<input type="hidden" name="matchtype" value="<?php echo ($matchtype != "") ? $matchtype : "" ?>">
+										<?php
+									}//endif
+									?>
+									<?php
+									if($keyword != "") {
+										?>
+										<input type="hidden" name="keyword" value="<?php echo ($keyword != "") ? $keyword : "" ?>">
+										<?php
+									}//endif
+									?>
+
+									<?php
+									if($bannersize != "") {
+										?>
+										<input type="hidden" name="bannersize" value="<?php echo ($bannersize != "") ? $bannersize : "" ?>">
+										<?php
+									}//endif
+									?>
+									<?php
+									if($site != "") {
+										?>
+										<input type="hidden" name="site" value="<?php echo ($site != "") ? $site : "" ?>">
+										<?php
+									}//endif
+									?>
+									<?php
+									if($placementid != "") {
+										?>
+										<input type="hidden" name="placementid" value="<?php echo ($placementid != "") ? $placementid : "" ?>">
+										<?php
+									}//endif
+									?>
+									<?php
+									if($segmentid != "") {
+										?>
+										<input type="hidden" name="segmentid" value="<?php echo ($segmentid != "") ? $segmentid : "" ?>">
+										<?php
+									}//endif
+									?>
+									<input type="hidden" name="trigKey" value="<?php echo ($trigKey != "") ? $trigKey : "" ?>">
+									<input type="hidden" name="trigger" value="yes">
+									<input type="hidden" name="rec" id="rec" value="">
+
+									<div class="div-email"><input placeholder="Enter E-mail Address" type="text" name="email" id="email" size="30" value=""></div>
+
+									<div class="clearMe">
+										<div class="div-zip"><input placeholder="Zip Code" type="text" name="zip" id="zip" size="30" value=""></div>
+										<div class="div-coupon_submit">
+											<button type="submit" class="btn-white">
+												Print My Coupon<span class="arrow"></span>
+											</button>
+										</div>
+									</div>
+
+									<div class="unsubChk"><input type="checkbox" name="unsubChk" id="unsubChk" value="Yes"></div>
+									<div class="unsub_txt">Yes, send me monthly emails with delicious recipes, coupon savings and exclusive contests. (<a href="http://www.dreamfieldsfoods.com/privacy-policy.php" target="_blank">Your email is safe with us</a>)</div>
+								</form>
+							</div>
+						</div>
+						<div class="form_disclaimer">
+							By entering your email address and checking the box above, you are agreeing to receive a monthly email from Dreamfields containing delicious recipes and coupon savings. You can unsubscribe at any time.
+						</div>
+
+				</div>
+
+			</div><!-- end .outer_greenbox -->
+			<div class="ca_2 ca_special">
+
+				<div class="ftr_thumbs">
+					<a href="http://dreamfieldsfoods.com/healthy-pasta-recipes.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_BrowseRecipes.gif" alt="Browse Recipes" title="Browse Recipes" />
+							<span>Browse <br class="show_mobile">Recipes</span>
+						</div>
+					</a>
+					<a href="http://dreamfieldsfoods.com/find-dreamfields-pasta.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_FindAStore.gif" alt="Find A Store" title="Find A Store" />
+							<span>Find A <br class="show_mobile">Store</span>
+						</div>
+					</a>
+					<a href="http://www.buydreamfields.com/" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_BuyOnline.gif" alt="Buy Online" title="Buy Online" />
+							<span>Buy Online</span>
+						</div>
+					</a>
+					<a href="http://dreamfieldsfoods.com/healthy-carb-pasta.php" target="_blank">
+						<div>
+							<img src="../_img/DRM259/DRM259_ftr_LearnMore.gif" alt="Learn More" title="Learn More" />
+							<span>Learn More</span>
+						</div>
+					</a>
+				</div><!-- ftr_thumbs -->
+
+
+			</div><!-- ca_2 ca_special -->
+
+
+		</div>
+		<!-- END page content -->
+
+
+	</div><!-- #border_gold -->
+	</div><!-- #site_body -->
+
+	<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
+<?php include('../_includes/DRM259/subdir_footer.php'); ?>
+</body>
+</html>
